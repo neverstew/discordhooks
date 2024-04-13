20aa32f338ce040867e9d918d60faafaf62e6cc3
Matt Williams <neverstew@duck.com>
2024-04-13 04:54:15 +0000

    Ask for all intents, log more

--- index.ts
+++ index.ts
@@ -1,22 +1,26 @@
 import { WebSocketManager, WebSocketShardEvents, CompressionMethod } from '@discordjs/ws';
 import { REST } from '@discordjs/rest';
 
+const ALL_INTENTS = 3276541;
+
 const rest = new REST().setToken(process.env.DISCORD_TOKEN);
 
-// This example will spawn Discord's recommended shard count, all under the current process.
 const manager = new WebSocketManager({
   token: process.env.DISCORD_TOKEN,
-  intents: 112192, // see https://discord-intents-calculator.vercel.app/
+  intents: process.env.INTENTS || ALL_INTENTS,
   rest,
   // compression: CompressionMethod.ZlibStream,
 });
 
 manager.on(WebSocketShardEvents.Dispatch, async (event) => {
+  const t = event?.data?.t;
+  const s = event?.data?.s;
+  console.info(`${s}: ${t} event dispatching...`);
   await fetch(process.env.ENDPOINT, {
      method: "POST",
      body: JSON.stringify(event),
   })
-  console.info("event dispatched");
+  console.info(`${s}: ${t} event dispatched.`);
 });
 
 await manager.connect();
